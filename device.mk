ea2891f5d6b74a4054ba5d1267a6e4eb3d7b7a9f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-05-19 10:42:27 +0530

    fog: Add bluetooth a2dp audio routes to audio policy config
    needed for dolby atmos playback
    
    Change-Id: I1e60db9117f00bb70511726012d739ae1f687b09
    Signed-off-by: UnmoveD <unmoved2121@gmail.com>

--- device.mk
+++ device.mk
@@ -74,6 +74,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.pro.xml \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/bluetooth_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/bluetooth_hearing_aid_audio_policy_configuration.xml \
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
