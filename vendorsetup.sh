4376267693d6e9e1618f23199527fb540eb9cf8d
alternoegraha <noegrahachan@gmail.com>
2024-09-03 04:01:02 +0000

    fog: vendorsetup: Switch to Luna kernel
    - kernel by @Asyanx
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>
    Signed-off-by: Iverz <iverzasterix@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,21 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
+<<<<<<< HEAD
 git clone https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
 git clone --depth=1 -b reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
+=======
+git clone -b fourteen https://github.com/asterixiverz/vendor_xiaomi_fog vendor/xiaomi/fog
+
+# Clone kernel tree
+git clone -b luna-staging https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 kernel/xiaomi/fog
+cd kernel/xiaomi/fog
+git reset --hard 036a81dd0ad2e9b8d618d0751c50bf0ed0a66eea
+git revert --no-edit df6112d9b117bc4592a64e3cdf7883da033d42d0
+cd ../../..
+>>>>>>> 52ed064 (fog: vendorsetup: Switch to Luna kernel)
 
 # Clone LineageOS' hardware/xiaomi
 git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
