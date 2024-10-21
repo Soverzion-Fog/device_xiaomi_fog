51659bf49801c8415bc4db9418967d38d94e3bc5
Drenzzz <116653610+drenzzz@users.noreply.github.com>
2024-10-22 00:33:40 +0700

    fog: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,20 +2,16 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone https://github.com/Soverzion-HQ/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b fourteen https://github.com/Soverzion-HQ/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
-git clone -b luna-staging https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 kernel/xiaomi/fog
-cd kernel/xiaomi/fog
-git reset --hard 036a81dd0ad2e9b8d618d0751c50bf0ed0a66eea
-git revert --no-edit df6112d9b117bc4592a64e3cdf7883da033d42d0
-cd ../../..
+git clone --depth=1 -b another-reset https://github.com/alternoegraha/wwy_kernel_xiaomi_fog_rebase kernel/xiaomi/fog
 
 # Clone MiuiCamera
 git clone -b uvite-sm6225 https://gitlab.com/soverzion-hq/proprietary_vendor_xiaomi_camera vendor/xiaomi/camera
 
 # Clone LineageOS' hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone -b fog-lineage-21 https://github.com/Soverzion-HQ/hardware_xiaomi hardware/xiaomi
 
 # Kernel fixups
 cd kernel/xiaomi/fog
