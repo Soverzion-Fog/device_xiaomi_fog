55b2c62ae9705e9b1d94300d7c5ef173db40523d
Drenzzz <realdrenzzz@gmail.com>
2025-01-09 16:23:06 +0000

    fog: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,13 +2,13 @@
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Clone vendor tree
-git clone -b pixelage https://github.com/alternoegraha/vendor_xiaomi_fog vendor/xiaomi/fog
+git clone -b 15 https://github.com/Soverzion-Fog/vendor_xiaomi_fog vendor/xiaomi/fog
 
 # Clone kernel tree
 git clone --depth=1 -b fog-r-oss-staging https://github.com/alternoegraha/fog_kernel_xiaomi_sm6225 kernel/xiaomi/fog
 
 # Clone LineageOS' hardware/xiaomi
-git clone -b lineage-22.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
+git clone -b 15.0 https://github.com/crdroidandroid/android_hardware_xiaomi hardware/xiaomi
 
 # Clone MiuiCamera
 git clone -b uvite-sm6225 https://gitlab.com/soverzion-hq/proprietary_vendor_xiaomi_camera vendor/xiaomi/camera
